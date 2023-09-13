0407f315caea3b0783e4cea6c0a0cdb6306d98e2
Omxarhack <L21170203@culiacan.tecnm.mx>
2023-09-12 22:01:31 -0700

    FALTA MENU Y CONTACTAME TAMBIEN FALTA ARRRGLAR BUG

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,13 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir-menu");
-const cerrar = document.querySelector("#cerrar");
+const nav = document.querySelector("nav");
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+const abrir = document.querySelector("#abrir-menu");
+const cerrar = document.querySelector("#cerrar-menu");
+
+    abrir.addEventListener("click", () => {
+        nav.setAttribute("id", "navvv");
+       console.log("abrir");
+    })
+    cerrar.addEventListener("click", ()=> {
+        nav.setAttribute("id","nav");
+        console.log("cerrar");   
+    })
